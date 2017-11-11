cee62623684e8a5964e7767406b78d630f0eaa46
heidthecamp <heidthecamp@gmail.com>
2017-11-11 16:41:57 -0500

    Moved where my SCSS folder was located and updated my gulpfile

--- themes/heidthecamp/gulpfile.js
+++ themes/heidthecamp/gulpfile.js
@@ -1,24 +1,17 @@
 'use strict';
 
 var gulp      = require('gulp'),
-    compass   = require('gulp-compass'),
-    sass      = require('gulp-sass'),
-    path      = require('path');
+    sass      = require('gulp-sass');
 
-gulp.task('build',['styles'])
-
-gulp.task('default', ['styles'], function(){
-  gulp.start('build')
-  //return gulp.watch('static/scss/**/*.scss', ['styles'])
+gulp.task('default', ['sass'], function(){
 });
 
-gulp.task('styles', function() {
-  return gulp.src('./static/scss/**/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: 'static/css',
-      sass: 'static/scss',
-      image: 'static/images'
-    }))
-    .pipe(gulp.dest('static/css'))
+gulp.task('sass', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError)
+    .pipe(gulp.dest('./static/css')))
 });
+
+gulp.task('sass:watch', function (){
+  gulp.watch('./scss/**/*.scss', ['sass'])
+})
